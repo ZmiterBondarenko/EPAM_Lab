e20d867c4628b13068831be9b6d7044b1bd3f222
Dmitry Bondarenko <zmiter.bondarenko@gmail.com>
2021-06-21 00:24:19 +0300

    Add getter, setter. Changed methods hasCode, equals, toString.

--- tasks-stage1/src/main/java/by/epam/cleancode/planes/experimentalPlane.java
+++ tasks-stage1/src/main/java/by/epam/cleancode/planes/experimentalPlane.java
@@ -1,20 +1,28 @@
 package by.epam.cleancode.planes;
 import by.epam.cleancode.models.ClassificationLevel;
 import by.epam.cleancode.models.ExperimentalTypes;
+import java.util.Objects;
 
-public class experimentalPlane extends Plane {
+public class ExperimentalPlane extends Plane {
 
     private ExperimentalTypes type;
     private ClassificationLevel classificationLevel;
 
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.type = type;
         this.classificationLevel = classificationLevel;
     }
 
+    public ExperimentalTypes getType() {
+        return type;
+    }
     public ClassificationLevel getClassificationLevel(){
         return classificationLevel;
+    }
+
+    public void setType(ExperimentalTypes type) {
+        this.type = type;
     }
 
     public void setClassificationLevel(ClassificationLevel classificationLevel){
@@ -23,18 +31,22 @@ public class experimentalPlane extends Plane {
 
     @Override
     public boolean equals(Object o) {
-        return super.equals(o);
+        if (this == o) return true;
+        if (!(o instanceof ExperimentalPlane)) return false;
+        if (!super.equals(o)) return false;
+        ExperimentalPlane experimentalPlane = (ExperimentalPlane) o;
+        return type == experimentalPlane.type && classificationLevel == experimentalPlane.classificationLevel;
     }
 
     @Override
     public int hashCode() {
-        return super.hashCode();
+        return Objects.hash(super.hashCode(), type, classificationLevel);
     }
 
     @Override
     public String toString() {
-        return "experimentalPlane{" +
-                "model='" + model + '\'' +
-                '}';
+        return super.toString().replace("}",
+                ", type=" + type + ", classification level=" + classificationLevel +
+                        '}');
     }
 }
